0a511b620607781a6f8a0ec6e8223e9abb1a9b3a
Ragul-05 <ragulgopinath2005@gmail.com>
2024-11-12 18:18:07 +0530

    Added the Basics Recursion concepts in DSA

---
+++ Recursion/PrintingNtimes.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class PrintingNtimes {
+    static void func(int i, int n) {
+        if(i > n) return;
+        System.out.println("Raj");
+        func(i + 1, n);
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        func(1, n);
+    }
+}
+
