68cb439f3c1f56d361c8808f363fda186cb25f81
Ragul-05 <ragulgopinath2005@gmail.com>
2024-12-07 18:07:33 +0530

    Added the file to Insert the string

---
+++ Strings/Insert.java
@@ -0,0 +1,27 @@
+package Strings;
+
+class GfG {
+    static String insertChar(String s, char c, int pos) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+      
+            
+            if (i == pos) 
+                res.append(c);
+      
+         
+            res.append(s.charAt(i));
+        }
+  
+        
+        if (pos >= s.length()) 
+            res.append(c);
+      
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "Geeks";
+        System.out.println(insertChar(s, 'A', 3));
+    }
+}
