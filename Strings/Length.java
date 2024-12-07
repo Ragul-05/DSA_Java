d993c7656498ad8d6cd77a02e560efae5ae6f580
Ragul-05 <ragulgopinath2005@gmail.com>
2024-12-07 18:06:15 +0530

    Added the file for finding the length of the string

---
+++ Strings/Length.java
@@ -0,0 +1,11 @@
+package Strings;
+
+import java.io.*;
+
+class Length {
+    public static void main(String[] args)
+    {
+        String s = "lets begin";
+        System.out.println(s.length());
+    }
+}
