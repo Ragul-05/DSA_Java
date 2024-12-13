04fd2021d890c4b8de510957f8175b35c1c36718
Ragul-05 <ragulgopinath2005@gmail.com>
2024-12-13 17:11:06 +0530

    Added the file for the traverse of singlylinkedlist

---
+++ LinkedList/SinglyLinkedList/Traverse.java
@@ -0,0 +1,60 @@
+package LinkedList.SinglyLinkedList;
+
+// Node class
+class Node {
+    int data;
+    Node next;
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// Singly Linked List class
+class SinglyLinkedList {
+    Node head;
+
+    // Add a new node to the end of the list
+    void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Traverse and print the list
+    void traverse() {
+        if (head == null) {
+            System.out.println("The list is empty.");
+            return;
+        }
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        // Adding elements to the list
+        list.append(10);
+        list.append(20);
+        list.append(30);
+        list.append(40);
+
+        // Traversing the list
+        System.out.println("Traversing the list:");
+        list.traverse();
+    }
+}
