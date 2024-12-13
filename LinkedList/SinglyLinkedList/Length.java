9ddc9643b29a02e72cff695230fbd9d44d773d62
Ragul-05 <ragulgopinath2005@gmail.com>
2024-12-13 17:40:29 +0530

    Added the file to calculate the length of the singlylinkedlist

---
+++ LinkedList/SinglyLinkedList/Length.java
@@ -0,0 +1,56 @@
+package LinkedList.SinglyLinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class SinglyLinkedList {
+    Node head;
+
+   
+    void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    
+    int getLength() {
+        int length = 0;
+        Node current = head;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+  
+        list.append(10);
+        list.append(20);
+        list.append(30);
+        list.append(40);
+
+        System.out.println("Length of the list:"+ list.length());
+        
+    }
+}
