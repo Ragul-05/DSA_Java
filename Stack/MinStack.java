73bbcca5d78784a8a97e1a452da44a0a63e55f3d
Ragul-05 <ragulgopinath2005@gmail.com>
2024-12-27 14:27:53 +0530

    Added the file to find out the minimum element in the stack

---
+++ Stack/MinStack.java
@@ -0,0 +1,55 @@
+package Stack;
+
+import java.util.Stack;
+class MinStack {
+    Stack < Long > st = new Stack < Long > ();
+    Long mini;
+    public MinStack() {
+        mini = Long.MAX_VALUE;
+    }
+    
+    public void push(int val) {
+        Long value = Long.valueOf(val);
+        if (st.isEmpty()) {
+            mini = value;
+            st.push(value);
+        } else {
+            if (value < mini) {
+                st.push(2 * value - mini);
+                mini = value;
+            } else {
+                st.push(value);
+            }
+        }
+    }
+    
+    public void pop() {
+        if (st.isEmpty()) return;
+
+        Long value = st.pop();
+        if (value < mini) {
+            mini = 2 * mini - value;
+        }
+    }
+    
+     public int top() {
+        Long val = st.peek();
+        if (val < mini) {
+            return mini.intValue();
+        }
+        return val.intValue();
+    }
+
+    public int getMin() {
+        return mini.intValue();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
