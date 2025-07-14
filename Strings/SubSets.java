d393856b90ffe1fda62722481fbed994cfc7f356
Ragul-05 <ragulgopinath2005@gmail.com>
2025-07-14 19:02:09 +0530

    Added the subsets.java file for counting the number of subsets

--- Strings/SubSets.java
+++ Strings/SubSets.java
@@ -2,13 +2,13 @@ package strings;
 
 public class SubSets {
     public static void main(String[] args) {
-        subSeq("", "abc");
-
-        // This will print all subsets of the string "abc"
-
+        //subSeq("", "abc");
+        String str = "abc";
+        int count = countSubSets(str,0);
+        System.out.println("Total subsets:"+ count);
     }
 
-    static void subSeq(String p , String up){
+    /*static void subSeq(String p , String up){
         if(up.isEmpty()){
             System.out.println(p);
             return;
@@ -16,5 +16,16 @@ public class SubSets {
         char ch = up.charAt(0);
         subSeq(p + ch , up.substring(1));
         subSeq(p,up.substring(1));
+    }*/
+
+    static int countSubSets(String str, int ind){
+        if(ind == str.length()){
+            return 1;
+        }
+        int include = countSubSets(str, ind + 1);
+        int exclude = countSubSets(str, ind + 1);
+        return include + exclude;
     }
+
+
 }
